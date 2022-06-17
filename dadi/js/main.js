4c7bea8f6d199e44983ef06b5bd82eaccc0cf4ec
Bice93 <bianca.cerulo@hotmail.it>
2022-06-17 17:55:45 +0200

    dadi DOM

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -10,10 +10,32 @@ console.log(`Giocatore` + " - " + numberPlayer);
 const numberPc = Math.floor((Math.random() * 6) + 1);
 console.log(`Pc` + " - " + numberPc);
 
+
+const containerNumber = document.getElementById(`container_number_player`);
+const containerNumberPc = document.getElementById(`container_number_pc`);
+const containerWinner = document.getElementById(`box_winner`);
+
+const numberPlayerBox = document.createElement(`div`);
+numberPlayerBox.innerHTML=numberPlayer;
+numberPlayerBox.style.textAlign="center";
+
+const numberPcBox = document.createElement(`div`);
+numberPcBox.innerHTML=numberPc;
+numberPcBox.style.textAlign="center";
+
+const boxWinner = document.createElement(`div`);
+boxWinner.style.textAlign="center";
+
 // 3) stabilisco il vincitore in base al punteggio più alto
     // ...quindi mi chiedo se il numero del giocatore e più alto ha vinto il giocatore altrimenti ha vinto il pc
 if (numberPlayer > numberPc) {
+    boxWinner.innerHTML= `Wow ha vinto il giocatore! Complimenti!`;
     console.log(`Wow ha vinto il giocatore! Complimenti!`);
 } else {
     console.log(`Wow ha vinto il Pc! Complimenti`);
+    boxWinner.innerHTML= `Wow ha vinto il Pc! Complimenti!`;
 }
+
+containerNumber.append(numberPlayerBox);
+containerNumberPc.append(numberPcBox);
+containerWinner.append(boxWinner);
