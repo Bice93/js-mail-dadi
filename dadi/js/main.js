7dae9b1910601866ac97693bf332bc77d4acc185
Bice93 <bianca.cerulo@hotmail.it>
2022-06-17 16:42:09 +0200

    Close Dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -4,8 +4,16 @@
 
 //1) Inizio con il generare numeri random da 1 a 6 per il giocatore
 const numberPlayer = Math.floor((Math.random() * 6) + 1);
-console.log(numberPlayer);
+console.log(`Giocatore` + " - " + numberPlayer);
 
 // 2) genero numeri random anche per il computer
 const numberPc = Math.floor((Math.random() * 6) + 1);
-console.log(numberPc);
+console.log(`Pc` + " - " + numberPc);
+
+// 3) stabilisco il vincitore in base al punteggio più alto
+    // ...quindi mi chiedo se il numero del giocatore e più alto ha vinto il giocatore altrimenti ha vinto il pc
+if (numberPlayer > numberPc) {
+    console.log(`Wow ha vinto il giocatore! Complimenti!`);
+} else {
+    console.log(`Wow ha vinto il Pc! Complimenti`);
+}
